38a19e22e18cc53ca27da4bc6a342d94b806778c
anubhutigupta2409 <mini.anu2409@gmail.com>
2022-12-30 14:07:34 +0530

    test commit

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -2,11 +2,7 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { oauthAuthorizationUrl } from "@octokit/oauth-authorization-url";
 import { createOAuthUserAuth } from "@octokit/auth-oauth-user";
 import { Octokit } from "octokit";
-<<<<<<< HEAD
 import * as templateRepo from  "../../templateRepo.json";
-=======
-import * as github_settings from  "../../github_settings.json";
->>>>>>> f3a445f (Basic Functionality implemented)
 
 
 
@@ -27,23 +23,16 @@ export class GithubService {
         return url;
     }
 
-<<<<<<< HEAD
     //function to be executed once the user authorises at github login interface
     async callback(code: string) : Promise<string>
     {
 
         //authentication strategy
-=======
-    //authentication strategy
-    async callback(code: string) : Promise<string>{
-
->>>>>>> f3a445f (Basic Functionality implemented)
         const auth = createOAuthUserAuth({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
             code,
         });
-<<<<<<< HEAD
 
         // Exchanges the code for the user access token authentication on first call
         // and caches the authentication for successive calls
@@ -70,22 +59,6 @@ export class GithubService {
         }
 
         return res.data.html_url;//url to the newly created repository
-=======
-        const { token } = await auth();
-        const octokit = new Octokit({ auth: token });
-        const res = await octokit.rest.repos.createUsingTemplate({
-            template_owner: github_settings.template_owner,
-            template_repo: github_settings.template_repo,
-            name: github_settings.new_repo_name,
-        });
-        if (res.status !== 201) {
-            throw new HttpException(
-                "Some error occured while creating the repo",
-                HttpStatus.FAILED_DEPENDENCY,
-            );
-        }
-        return res.data.html_url;
->>>>>>> f3a445f (Basic Functionality implemented)
 
     }
 
